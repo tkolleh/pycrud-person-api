ba8cddfb8a682fb0ec9254815960c1824e95bc78
tkolleh <tinatuh@gmail.com>
2020-11-09 16:07:22 -0500

    Marshal person with jsonify

--- src/person_api/controller.py
+++ src/person_api/controller.py
@@ -1,7 +1,7 @@
 from flask import request, jsonify
 from flask_cors import CORS
 from flask_restful import Resource, Api, abort
-from person_api.services.person import get_persons, get_person
+from person_api.services.person import get_persons, get_person, new_person
 
 
 class PersonListResource(Resource):
@@ -15,10 +15,11 @@ class PersonResource(Resource):
         person = get_person(person_id)
         if person is None:
             abort(404, message="Person with id '{}' not found".format(person_id))
-        return person
+        return jsonify(person)
 
     def put(self, person_id: str):
         payload = request.json
+        new_person(payload['fname'], payload['mname'], payload['lname'], payload['email'], payload['age'])
 
 
 def init_controller(app):
